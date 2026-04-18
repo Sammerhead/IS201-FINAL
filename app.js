8f3995df065c55fca9850565f2cdd2300b7116f6
Sammerhead <sb488215@byu.edu>
2026-04-18 12:43:04 -0600

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ function calculateStrength() {
     let ratio = liftWeight / bodyweight;
     let tier = "";
 
+    // BENCH
     if (liftType === "bench") {
         if (ratio < 0.75) {
             tier = "Bronze";
@@ -24,6 +25,44 @@ function calculateStrength() {
         } else if (ratio < 2.0) {
             tier = "Elite";
         } else if (ratio < 2.25) {
+            tier = "You Are Him 😤";
+        } else {
+            tier = "What are you, Ronnie Coleman?? 💀";
+        }
+    }
+
+    // SQUAT
+    else if (liftType === "squat") {
+        if (ratio < 1.0) {
+            tier = "Bronze";
+        } else if (ratio < 1.5) {
+            tier = "Silver";
+        } else if (ratio < 2.0) {
+            tier = "Gold";
+        } else if (ratio < 2.5) {
+            tier = "Platinum";
+        } else if (ratio < 3.0) {
+            tier = "Elite";
+        } else if (ratio < 3.5) {
+            tier = "You Are Him 😤";
+        } else {
+            tier = "What are you, Ronnie Coleman?? 💀";
+        }
+    }
+
+    // DEADLIFT
+    else if (liftType === "deadlift") {
+        if (ratio < 1.25) {
+            tier = "Bronze";
+        } else if (ratio < 1.75) {
+            tier = "Silver";
+        } else if (ratio < 2.25) {
+            tier = "Gold";
+        } else if (ratio < 2.75) {
+            tier = "Platinum";
+        } else if (ratio < 3.25) {
+            tier = "Elite";
+        } else if (ratio < 3.75) {
             tier = "You Are Him 😤";
         } else {
             tier = "What are you, Ronnie Coleman?? 💀";
